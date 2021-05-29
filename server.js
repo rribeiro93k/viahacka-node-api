8f5af5b896e5d8e1bfa543ebc2d55a9fa45d658e
Rafael Ribeiro <rafael.aribeiro@viavarejo.com.br>
2021-05-29 18:25:34 -0300

    feature: db2 endpoint connection

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const compression = require('compression');
 const cors = require('cors');
 const app = express();
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+const ibmdb = require("ibm_db");
+const connStr = "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-14.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=xrz69130;PWD=44k8g5m12^1sg0sc;";
 
 app.use(cors({ origin: true }));
 app.use(compression());
@@ -34,12 +37,32 @@ app.get('/', (req, res) => {
   res.status(200).send({ message: 'Hacka Node API' });
 });
 
+app.get('/db2-connect', (req, res) => {
+
+  ibmdb.open(connStr, (err, connection) => {
+    if (err) {
+      res.status(500).send({ error: err });
+      return;
+    }
+    connection.query("select * from FAKE_TABLE_TEST", (err1, rows) => {
+      if (err1) {
+        res.status(500).send({ error: err1 });
+      } else {
+        res.status(200).send(rows);
+      }
+      connection.close((err2) => {
+        if (err2) res.status(500).send({ error: err2 });
+      });
+    });
+  });
+});
+
 // Routes
 const users = require('./routes/users');
 app.use('/users', users);
 
 app.listen(port, () => {
-    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+  console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
 
 
